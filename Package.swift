7f67c8aba627307a8748b75a0e1f6981c448429d
Carl Chen <ccf.developer@gmail.com>
2017-11-30 19:09:57 +0800

    Add xcodeproj

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/onevcat/Rainbow", from: "3.0.0"),
         .package(url: "https://github.com/ReactiveX/RxSwift.git", "4.0.0" ..< "5.0.0"),
     ],
     targets: [
@@ -15,6 +16,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "IronBank",
+            dependencies: ["Rainbow", "IronBankKit"]),
+        .target(
+            name: "IronBankKit",
             dependencies: []),
     ]
 )
--- Sources/IronBank/main.swift
+++ Sources/IronBank/main.swift
@@ -1,1 +1,23 @@
-print("Hello, world!")
+import Foundation
+import Rainbow
+
+#if !os(macOS)
+print("IronBank only support for macOS now.".red)
+exit(EX_OSERR)
+#endif
+
+// check command argument
+let args = CommandLine.arguments
+guard args.count == 2 && args[1] == "install" else {
+    print("See usage of IronBank.".red)
+    exit(EX_USAGE)
+}
+let currentPath = FileManager.default.currentDirectoryPath
+
+
+// check configuation file
+
+
+// do the right thing
+
+
